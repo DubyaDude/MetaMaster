7ca4328187ff91f70589bd8ad491bb9590336250
Usman Shafiq <ushafiq141@gmail.com>
2023-06-03 22:46:20 -0400

    Add project files.

---
+++ MetaMaster/Program.cs
@@ -0,0 +1,41 @@
+﻿using MetadataExtractor;
+using System;
+using System.Linq;
+
+namespace MetaMaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("File not specified");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(args[0]);
+
+            try
+            {
+                var items = ImageMetadataReader.ReadMetadata(args[0]).GroupBy(x => x.Name);
+
+                foreach (var directory in items)
+                {
+                    Console.WriteLine(Environment.NewLine + directory.Key);
+                    foreach (var tag in directory.SelectMany(x => x.Tags))
+                        Console.WriteLine($" => {tag.Name} = {tag.Description}");
+                }
+            }
+            catch (MetadataExtractor.ImageProcessingException exception)
+            {
+                Console.WriteLine($"Error reading item: {exception.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
